91308d96e17dcbd47c60d29f4e0fce2fd12884b0
Obeda Velonjatovo <obeda.velonjatovo@sjsu.edu>
2022-04-01 20:53:28 -0700

    Count the number of Character in a string using hashmap, then iterate through the string while checking for the first key match with value 1 in the hashMap

---
+++ String/First Unique Character in a String/src/Main.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        firstUniqChar("leetcode");
+
+    }
+
+    public static int firstUniqChar(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(map.containsKey(ch))
+                map.put(ch, map.get(ch)+1);
+            else
+                map.put(ch, 1);
+
+        }
+
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(map.get(ch) == 1)
+                return i;
+        }
+
+        return -1;
+
+    }
+
+
+}
+
