371f8d39b2bf1d1a8fc132b2e42695703ca75453
Obeda Velonjatovo <obeda.velonjatovo@sjsu.edu>
2022-04-28 14:15:41 -0700

    added as a comment the best solution found

--- String/Valid Anagram/src/Main.java
+++ String/Valid Anagram/src/Main.java
@@ -5,8 +5,18 @@ public class Main {
     public static void main(String[] args) {
         String str1 = "rat";
         String str2 = "car";
-        System.out.println(isAnagram(str1, str2));
+        System.out.println('z' - 'a');
+        //System.out.println(isAnagram(str1, str2));
     }
+
+    // Best solution
+    /*public boolean isAnagram(String s, String t) {
+        int[] alphabet = new int[26];
+        for (int i = 0; i < s.length(); i++) alphabet[s.charAt(i) - 'a']++;
+        for (int i = 0; i < t.length(); i++) alphabet[t.charAt(i) - 'a']--;
+        for (int i : alphabet) if (i != 0) return false;
+        return true;
+    }*/
 
     public static boolean isAnagram(String s, String t) {
 
