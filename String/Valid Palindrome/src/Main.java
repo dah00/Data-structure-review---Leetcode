b448dd494b22c46595b397d2b447d438794b39f2
Obeda Velonjatovo <obeda.velonjatovo@sjsu.edu>
2022-04-28 14:33:13 -0700

    2. remove all non-alphanumeric characters

---
+++ String/Valid Palindrome/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+        String s = "ObeDGSa fasd78,42&*& LFiuba";
+        s = s.replaceAll("[^a-zA-Z0-9]", "");
+        s = s.toLowerCase();
+        System.out.println(s);
+    }
+
+    public static boolean isPalindrome(String s) {
+        /*
+            2. remove all non-alphanumeric characters
+            1. Convert all uppercase into lowercase
+            3. Remove all white space
+            4. Reverse s and put in another string
+            5. Compare the two strings
+         */
+        s = s.replaceAll("[^a-zA-Z0-9]", "");
+        s = s.toLowerCase(Locale.ROOT);
+
+        String reversedS = new StringBuilder(s).reverse().toString();
+
+        return s.equals(reversedS);
+    }
+}
