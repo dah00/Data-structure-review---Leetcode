83266d82d43847cf5b64aec731599c57f8a04155
Obeda Velonjatovo <obeda.velonjatovo@sjsu.edu>
2022-05-24 18:16:10 -0700

    Add a better solution as comment

--- Linked List/Delete Node in a Linked List/src/Main.java
+++ Linked List/Delete Node in a Linked List/src/Main.java
@@ -7,6 +7,13 @@ public class Main {
         move to the next node and repeat the process above until the node before the tail node
         assign the value of the tail node to the current node and set next to null
      */
+
+    /*
+        Better solution
+        node.val = node.next.val;
+        node.next = node.next.next;
+     */
+
     public void deleteNode(ListNode node) {
         while(node.next.next != null){
             node.val = node.next.val;
