a1decac64f806fdb88bbb7f63a755549a4a8f824
Obeda Velonjatovo <obeda.velonjatovo@sjsu.edu>
2022-05-24 18:11:24 -0700

    Deleting the given node without access to the head node.

---
+++ Linked List/Delete Node in a Linked List/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+    /*
+        assign the value of the next node to the current node's value
+        move to the next node and repeat the process above until the node before the tail node
+        assign the value of the tail node to the current node and set next to null
+     */
+    public void deleteNode(ListNode node) {
+        while(node.next.next != null){
+            node.val = node.next.val;
+            node = node.next;
+        }
+        node.val = node.next.val;
+        node.next = null;
+    }
+}
+
+class ListNode {
+     int val;
+     ListNode next;
+     ListNode(int x) { val = x; }
+}
