850b0ef3b6e5cdab1798739e3021bc6213714ce7
Obeda Velonjatovo <obeda.velonjatovo@sjsu.edu>
2022-05-26 12:14:07 -0700

    One pass: traversing the linked list one time only

--- Linked List/Remove Nth Node from End of List/src/Main.java
+++ Linked List/Remove Nth Node from End of List/src/Main.java
@@ -12,6 +12,9 @@ class ListNode {
     ListNode next;
     ListNode(int x) { val = x; }
 
+
+    // First Solution : brute force
+    /*
     public ListNode removeNthFromEnd(ListNode head, int n){
         // 1. Iterate through the linked list to get the length of the list
         // 2. Remove node at length-n
@@ -36,6 +39,46 @@ class ListNode {
         else
             curr.next = curr.next.next;
         return head;
+    }
+     */
+
+    // Second solution : 1 iteration
+    public ListNode removeNthFromEnd(ListNode head, int n){
+        // Have two pointers i and j : place i at the Nth position by using j
+        //  i starts at the head of the linked list
+        //  j is at i+n-th position
+        //      Check if j is null
+        //          True: delete node at i-th position
+        //          False: move i to the next position
+
+        /* TODO: Have the same concept but
+            1. have a newHead(0) pointing to the head
+            2. set the gap between iNode and jNode first
+            3. make iNode and jNode iterate through the linked list until jNode == null while keeping the gap between them.
+            4. Remember to stop at the node before the desired node instead of the stopping at the desired node
+        */
+
+        ListNode newHead = new ListNode(0);
+        newHead.next = head;
+        ListNode slow = newHead;
+        ListNode fast = slow;
+
+        for (int i=1; i<=n+1; i++)
+            fast = fast.next;
+
+        while (fast != null){
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        slow.next = slow.next.next;
+        return newHead.next;
+
+
+        // Consider an empty linked list
+        // Consider a single element list
+
+
     }
 
 }
