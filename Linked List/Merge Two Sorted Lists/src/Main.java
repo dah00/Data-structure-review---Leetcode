9e421dbe722d9c76aa7397f22da9272b7637aaee
Obeda Velonjatovo <obeda.velonjatovo@sjsu.edu>
2022-05-31 16:12:39 -0700

    Recursive solution

--- Linked List/Merge Two Sorted Lists/src/Main.java
+++ Linked List/Merge Two Sorted Lists/src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        ListNode list1 = new ListNode(1);
+        list1.next = new ListNode(2);
+        list1.next.next = new ListNode(4);
 
+        ListNode list2 = new ListNode(1);
+        list2.next = new ListNode(3);
+        list2.next.next = new ListNode(4);
+
+        ListNode.mergeTwoLists(list1, list2);
     }
 }
 
@@ -20,8 +28,27 @@ class ListNode {
         this.next = next;
     }
 
-    private ListNode mergeTwoLists(ListNode list1, ListNode list2){
-        ListNode newHead = null;
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2){
+
+        if(list1 == null)
+            return list2;
+        if(list2 == null){
+            return list1;
+        }
+        if(list1.val <= list2.val){
+            list1.next =  mergeTwoLists(list1.next, list2);
+            return list1;
+        }
+        else{
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
+        }
+
+
+
+        // Brute force solution
+
+        /*ListNode newHead = null;
         ListNode curr = null;
         while(list1 != null || list2 != null){
             if(list1 != null && list2 != null && list1.val <= list2.val){
@@ -74,6 +101,6 @@ class ListNode {
             }
         }
 
-        return newHead;
+        return newHead;*/
     }
 }
