722c8046a6ccb6811cb2e10b42933bf2ad20bd57
Obeda Velonjatovo <obeda.velonjatovo@sjsu.edu>
2022-06-13 14:09:54 -0700

    Reviewed solution after reviewing about how to solve recursive problem

--- Trees/Validate Binary Search tree/src/Main.java
+++ Trees/Validate Binary Search tree/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        TreeNode root = new TreeNode(2, new TreeNode(1), new TreeNode(3));
-        TreeNode.isValidBST(root);
+        Integer a = 2;
+
     }
 }
 
