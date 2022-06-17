5baa26658ed78f5ab95474d5bb0b8c86ee56d3d8
dah00 <61763829+dah00@users.noreply.github.com>
2022-06-17 12:24:58 -0700

    Update Main.java

--- Trees/Validate Binary Search tree/src/Main.java
+++ Trees/Validate Binary Search tree/src/Main.java
@@ -36,6 +36,45 @@ class TreeNode {
         prev = root.val;
         return inorder(root.right);
     }
+    
+    // 3rd solution: Iterative traversal valid range
+    /*
+        class Solution {
+
+    private Deque<TreeNode> stack = new LinkedList();
+    private Deque<Integer> upperLimits = new LinkedList();
+    private Deque<Integer> lowerLimits = new LinkedList();
+
+    public void update(TreeNode root, Integer low, Integer high) {
+        stack.add(root);
+        lowerLimits.add(low);
+        upperLimits.add(high);
+    }
+
+    public boolean isValidBST(TreeNode root) {
+        Integer low = null, high = null, val;
+        update(root, low, high);
+
+        while (!stack.isEmpty()) {
+            root = stack.poll();
+            low = lowerLimits.poll();
+            high = upperLimits.poll();
+
+            if (root == null) continue;
+            val = root.val;
+            if (low != null && val <= low) {
+                return false;
+            }
+            if (high != null && val >= high) {
+                return false;
+            }
+            update(root.right, val, high);
+            update(root.left, low, val);
+        }
+        return true;
+    }
+}
+    */
 
 
     // 1st solution: Recursive Traversal with Valid Range
