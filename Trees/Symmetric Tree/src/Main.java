ce93f7245677042ad79d1da41579606b62a184d1
Obeda Velonjatovo <obeda.velonjatovo@sjsu.edu>
2022-06-18 17:39:39 -0700

    Recursive solution

--- Trees/Symmetric Tree/src/Main.java
+++ Trees/Symmetric Tree/src/Main.java
@@ -27,45 +27,15 @@ class TreeNode {
     }
 
     public boolean isSymmetric(TreeNode root) {
-        if(root.left == null && root.right == null)
+        return help(root.left, root.right);
+    }
+
+    private boolean help(TreeNode left, TreeNode right){
+        // base case
+        if (left == null && right == null)
             return true;
-
-        Queue<TreeNode> queueTemp = new LinkedList<>();
-        Queue<TreeNode> queueStore = new LinkedList<>();
-
-        // Traversing the left side of the tree
-        if(root.left != null){
-            queueTemp.add(root.left);
-            queueStore.add(root.left);
-        }
-        while (!queueTemp.isEmpty()){
-            TreeNode node = queueTemp.poll();
-            if(node != null){
-                queueTemp.add(node.left);
-                queueTemp.add(node.right);
-                queueStore.add(node.left);
-                queueStore.add(node.right);
-            }
-        }
-
-        // Traversing the right side of the Tree
-        if (root.right != null){
-            queueTemp.add(root.right);
-            if (root.right.val != queueStore.poll().val)
-                return false;
-        }
-        while (!queueTemp.isEmpty()){
-            TreeNode node = queueTemp.poll();
-            if(node != null){
-                queueTemp.add(node.right);
-                queueTemp.add(node.left);
-                if(node.right.val != queueStore.poll().val)
-                    return false;
-                if (node.left.val != queueStore.poll().val)
-                    return false;
-            }
-        }
-        return true;
-
+        if(left == null || right == null || left.val != right.val)
+            return false;
+        return help(left.left, right.right) && help(left.right, right.left);
     }
 }
