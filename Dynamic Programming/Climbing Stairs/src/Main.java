626bc2e1c3699d760630d57f2152f7c33c6e1014
Obeda Velonjatovo <obeda.velonjatovo@sjsu.edu>
2022-07-12 14:16:00 -0700

    Second attempt failed

--- Dynamic Programming/Climbing Stairs/src/Main.java
+++ Dynamic Programming/Climbing Stairs/src/Main.java
@@ -6,9 +6,9 @@ public class Main{
         arr.add(1);
         arr.add(1);
         arr.add(2);
-        arr.add(2);
+        arr.add(3);
         System.out.println(arr.size());
-        System.out.println(arr);
+        System.out.println(arr.get(3));
     }
 
     public static int climbStairs(int n) {
@@ -37,16 +37,44 @@ public class Main{
                 2+1
                 2+2
 
+                Rules:
+                when size is reduced, there must be at least one 2
+                if(sum<n)
+                    add the number of two until (sum >= n) but keep the same size of array
+                    when size decrement, the number of 2s in the array increment
+
          */
         int count=0;
         ArrayList<Integer> arr = new ArrayList<>(n);
-        for(int i=1; i<=n; i++) {
+        for(int i=0; i<n; i++) {
             arr.add(i);
         }
         while(arr.size() >= (n/2)){
-
+            int ones = 0, twos = 0, sum = 0;
+            for(int i=0; i<arr.size(); i++){
+                if (arr.get(i) == 1)
+                    ones++;
+                else if (arr.get(i) == 2)
+                    twos++;
+                sum += arr.get(i);
+            }
+            if (sum == n){
+                count = helper(arr.size(), ones, twos);
+            }
+            else if(sum > n){
+                arr.remove(arr.size()-1);
+                arr.set(arr.size()-2, 2);   // double check
+            }
+            else if(sum < n){
+                while
+            }
 
         }
         return 0;
     }
+
+    private static int helper(int size, int ones, int twos){
+        return 0;
+    }
+
 }
