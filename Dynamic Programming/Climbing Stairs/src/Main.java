73e020dd88e893907b216c4e75cdabe31596f32f
Obeda Velonjatovo <obeda.velonjatovo@sjsu.edu>
2022-07-13 12:17:34 -0700

    Solved: Recursive approach, Recursion + memorization, Dynamic programming

--- Dynamic Programming/Climbing Stairs/src/Main.java
+++ Dynamic Programming/Climbing Stairs/src/Main.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main{
     public static void main(String[] args) {
@@ -44,37 +46,39 @@ public class Main{
                     when size decrement, the number of 2s in the array increment
 
          */
-        int count=0;
-        ArrayList<Integer> arr = new ArrayList<>(n);
-        for(int i=0; i<n; i++) {
-            arr.add(i);
-        }
-        while(arr.size() >= (n/2)){
-            int ones = 0, twos = 0, sum = 0;
-            for(int i=0; i<arr.size(); i++){
-                if (arr.get(i) == 1)
-                    ones++;
-                else if (arr.get(i) == 2)
-                    twos++;
-                sum += arr.get(i);
-            }
-            if (sum == n){
-                count = helper(arr.size(), ones, twos);
-            }
-            else if(sum > n){
-                arr.remove(arr.size()-1);
-                arr.set(arr.size()-2, 2);   // double check
-            }
-            else if(sum < n){
-                while
-            }
 
-        }
-        return 0;
+        // Recursive method
+        /*
+        if(n==0) return 0;
+        if(n==1) return 1;
+        if(n==2) return 2;
+        return climbStairs(n-1) + climbStairs(n-2);*/
+
+        // Dynamic programming
+        /*
+            if(n==1)
+                return 1;
+            int[] memo = new int[n];
+            memo[0] = 1;
+            memo[1] = 2;
+            for(int i=2; i<n; i++)
+                memo[i] = memo[i-2] + memo[i-1];
+            return memo[n-1];
+         */
+
+        // Memorization using HashMap
+        Map<Integer, Integer> memo = new HashMap<>();
+        memo.put(1,1);
+        memo.put(2,2);
+        return helper(n, memo);
     }
 
-    private static int helper(int size, int ones, int twos){
-        return 0;
+    private static int helper(int n, Map<Integer, Integer> memo){
+        if(memo.containsKey(n))
+            return memo.get(n);
+        memo.put(n, helper(n-1, memo) + helper(n-2, memo));
+        return memo.get(n);
     }
+
 
 }
