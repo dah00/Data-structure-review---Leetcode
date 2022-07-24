3a19e11fda9b90fc27b090fa89d1ef039dc3b667
Obeda Velonjatovo <obeda.velonjatovo@sjsu.edu>
2022-07-24 14:01:01 -0700

    Solved: Iterative + memo

--- Dynamic Programming/House Robber/src/Main.java
+++ Dynamic Programming/House Robber/src/Main.java
@@ -5,12 +5,12 @@ public class Main {
         }
     }
     private static int rob(int[] nums){
-        return helper(nums, nums.length - 1);
-    }
-    private static int helper(int[] nums, int i) {
-        if (i < 0) {
-            return 0;
+        int[] memo = new int[nums.length+1];
+        memo[0] = 0;
+        memo[1] = nums[0];
+        for(int i=1; i<nums.length; i++){
+            memo[i+1] =  Math.max(memo[i], memo[i-1] + nums[i]);
         }
-        return Math.max(helper(nums, i - 2) + nums[i], helper(nums, i - 1));
+        return memo[nums.length];
     }
 }
