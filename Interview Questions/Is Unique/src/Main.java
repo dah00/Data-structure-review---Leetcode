b91bc742f3d9a038b6c81f5cebb58ac6f8343b05
Obeda Velonjatovo <obeda.velonjatovo@sjsu.edu>
2022-07-25 10:12:18 -0700

    2 solutions: first using hashSet and traversing the rest of the characrer twice with O(n^2). Still need to find a solution without using a data structure and O(n)

---
+++ Interview Questions/Is Unique/src/Main.java
@@ -0,0 +1,32 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        String str = "velonjatidy";
+        System.out.println(isUnique(str));
+    }
+
+    private static boolean isUnique(String str){
+        // USING DATA STRUCTURE
+        /*Set<Character> set = new HashSet<>();
+        for(int i=0; i<str.length(); i++){
+            if(!set.contains(str.charAt(i)))
+                set.add(str.charAt(i));
+            else
+                return false;
+        }
+        return true;*/
+
+        // WITH NO DATA STRUCTURE USED O(n^2)
+        for(int i=0; i<str.length()-1; i++){
+            char chr_i = str.charAt(i);
+            for(int j=i+1; j<str.length(); j++){
+                char chr_j = str.charAt(j);
+                if (chr_i == chr_j)
+                    return false;
+            }
+        }
+        return true;
+    }
+}
